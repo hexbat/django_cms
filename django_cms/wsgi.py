3872d27b4812c975a6bab8c8e43de07e77eb4a4c
hexbat <hexbat@gmail.com>
2017-08-22 22:53:01 +0300

    example

--- django_cms/wsgi.py
+++ django_cms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_cms_example project.
+WSGI config for django_cms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_cms_example.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_cms.settings")
 
 application = get_wsgi_application()
